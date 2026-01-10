64afe7431c9c51e7c5d9729c9fffa00520bd86ff
Imam Bari Setiawan <imambarisetiawan695@gmail.com>
2026-01-10 21:13:00 +0700

    update component

---
+++ app/(landing)/components/ui/cart-popup.tsx
@@ -0,0 +1,109 @@
+import PriceFormmatter from "@/app/utils/price-formatter";
+import Image from "next/image";
+import Button from "./button";
+import { FiArrowRight, FiTrash2 } from "react-icons/fi";
+import { useRouter } from "next/navigation";
+
+export const cartList = [
+  {
+    name: "SportsOn Hyperfast Shoes",
+    category: "Running",
+    price: 329000,
+    qty: 2,
+    imgUrl: "product-3.svg",
+  },
+  {
+    name: "SportsOn Rockets Tennis",
+    category: "Tennis",
+    price: 999000,
+    qty: 3,
+    imgUrl: "product-2.svg",
+  },
+  {
+    name: "SportsOn Slowlivin",
+    category: "Running",
+    price: 119000,
+    qty: 2,
+    imgUrl: "product-5.svg",
+  },
+  {
+    name: "SportsOn Hyperfast Shoes",
+    category: "Running",
+    price: 329000,
+    qty: 1,
+    imgUrl: "product-3.svg",
+  },
+  {
+    name: "SportsOn Rockets Tennis",
+    category: "Tennis",
+    price: 999000,
+    qty: 2,
+    imgUrl: "product-2.svg",
+  },
+];
+
+const CartPopup = () => {
+  const { push } = useRouter();
+
+  const handleCheckout = () => {
+    push("/checkout");
+  };
+
+  const totalPrice = cartList.reduce(
+    (total, item) => total + item.price + item.qty,
+    0
+  );
+
+  return (
+    <div className="absolute bg-white right-0 top-12 shadow-xl shadow-black/10 border border-gray-200 w-90 z-10">
+      <div className="p-4 border-b border-gray-200 font-bold text-center">
+        Shopping Chart
+      </div>
+      {cartList.map((item, index) => (
+        <div className="border-b border-gray-200 p-4 flex gap-3" key={index}>
+          <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
+            <Image
+              src={`/images/products/${item.imgUrl}`}
+              width={63}
+              height={63}
+              alt={item.name}
+              className="aspect-square object-contain"
+            />
+          </div>
+          <div className="self-center">
+            <div className="text-sm font-medium">{item.name}</div>
+            <div className="flex gap-3 font-medium text-xs">
+              <div>{item.qty}</div>
+              <div className="text-primary">{PriceFormmatter(item.price)}</div>
+            </div>
+          </div>
+          <Button
+            size="small"
+            variant="ghost"
+            className="w-7 h-7 p-0! self-center ml-auto"
+          >
+            <FiTrash2 />
+          </Button>
+        </div>
+      ))}
+      <div className="border-t border-gray-200 p-4">
+        <div className="flex justify-between font-semibold">
+          <div className="text-sm">Total</div>
+          <div className="text-primary text-xs">
+            {PriceFormmatter(totalPrice)}
+          </div>
+        </div>
+        <Button
+          variant="dark"
+          size="small"
+          className="w-full mt-4"
+          onClick={handleCheckout}
+        >
+          Checkout Now <FiArrowRight />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default CartPopup;
